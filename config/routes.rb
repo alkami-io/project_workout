de3b8dc8de68ca253c1bd683ba1e3313efcc84b7
Robert Cornell <robert@alkami.io>
2017-07-02 17:09:34 -0600

    created user model and added devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # You can have the root of your site routed with "root"
